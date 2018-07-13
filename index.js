d7237d6da78f7b5191c4728820c12c85a93d2533
Suchitra Naik <susanaik@gmail.com>
2018-07-13 13:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function justInvoke(fn){
 
 }
+
+function setThisWithCall(fn, thisValue, arg){
+
+}
+
+function setThisWithApply(fn, thisValue, args){
+
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+
+}
